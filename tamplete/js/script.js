ed56f9fafc3dd02ed50426eb27fbe0c02116897d
Mustapha <56976902+MustaphaIdrissi@users.noreply.github.com>
2025-02-26 21:44:34 +0000

    Add files via upload

--- tamplete/js/script.js
+++ tamplete/js/script.js
@@ -22,3 +22,27 @@ function playVideo() {
         playButton.style.display = "flex";
     };
 }
+function playVideo2() {
+    let container = document.querySelector('.video-container1');
+    let thumbnail = document.querySelector('.thumbnail1');
+    let overlay = document.querySelector('.overlay1');
+    let playButton = document.querySelector('.play-button1');
+    let video = document.querySelector('.video1');
+
+    // إخفاء الصورة والتراكب وزر التشغيل
+    thumbnail.style.display = "none";
+    overlay.style.display = "none";
+    playButton.style.display = "none";
+
+    // عرض الفيديو وتشغيله
+    video.style.display = "block";
+    video.play();
+
+    // عند انتهاء الفيديو، إعادة الصورة والغلاف
+    video.onended = function() {
+        video.style.display = "none";
+        thumbnail.style.display = "block";
+        overlay.style.display = "block";
+        playButton.style.display = "flex";
+    };
+}
